a329ee392734487af4ba4c7afab4fbcc65b34e4f
Drahomira Prokopova <prokopova.drahomira@gmail.com>
2022-04-15 14:14:56 +0200

    Zofka kresli prasatko a tri obrazce

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -8,11 +8,66 @@ public class HlavniProgram {
         Turtle zofka;
 
         zofka = new Turtle();
-        zofka.setLocation(40, 200);
+        zofka.setLocation(50, 350);
 
         zofka.turnRight(90);
 
+        //Část 1
         nakresliPrasatko(zofka);
+
+        //Část 2
+        zofka.penUp();
+        zofka.turnLeft(180);
+        zofka.move(40);
+        zofka.turnRight(45);
+        zofka.move(180);
+
+        zofkaKresliOsmiuhelnik(zofka);
+
+        zofka.penUp();
+        zofka.turnRight(90);
+        zofka.move(40);
+        zofka.move(180);
+        zofka.penDown();
+        zofka.turnRight(135);
+
+        zofkaKresliKolecko(zofka);
+
+        zofka.penUp();
+        zofka.turnRight(180);
+        zofka.move(100);
+        zofka.penDown();
+
+        zofkaKresliSlunicko(zofka);
+    }
+
+    private void zofkaKresliSlunicko(Turtle zofka) {
+        for (int j = 0; j < 12; j++) {
+            for (int i = 0; i < 6; i++) {
+                zofka.move(4);
+                zofka.turnRight(5);
+            }
+            zofka.turnLeft(90);
+            zofka.move(25);
+            zofka.turnLeft(180);
+            zofka.move(25);
+            zofka.turnLeft(90);
+        }
+    }
+
+    private void zofkaKresliKolecko(Turtle zofka) {
+        for (int i = 0; i < 74; i++) {
+            zofka.move(4);
+            zofka.turnRight(5);
+        }
+    }
+
+    private void zofkaKresliOsmiuhelnik(Turtle zofka) {
+        for (int i = 0; i < 8; i++) {
+            zofka.penDown();
+            zofka.move(40);
+            zofka.turnRight(45);
+        }
     }
 
     private void nakresliPrasatko(Turtle zofka) {
